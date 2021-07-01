56d04e3dee1919b6f8fc99628dff800f667f5d3e
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-07-01 14:07:03 +0200

    move LinSIM to SIMDynElasticity.h
    need access to enable private template

--- SIMDynElasticity.h
+++ SIMDynElasticity.h
@@ -30,6 +30,21 @@
 
 
 /*!
+  \brief Linear quasi-static solution driver.
+*/
+
+class LinSIM : public NonLinSIM
+{
+public:
+  //! \brief The constructor forwards to the parent class constructor.
+  explicit LinSIM(SIMbase& sim) : NonLinSIM(sim,NonLinSIM::NONE_UPTAN) {}
+  //! \brief Empty destructor.
+  virtual ~LinSIM() {}
+};
+
+
+
+/*!
   \brief Driver class for dynamic elasticity problems with fracture.
 */
 
